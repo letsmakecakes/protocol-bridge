dbfc76775eaa4b03e8826cea0c751b7228ef2a3c
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2023-05-19 22:53:05 +0530

    Update flight_controller.py

--- flight_controller.py
+++ flight_controller.py
@@ -1,37 +1,15 @@
-import time
 from pymavlink import mavutil
 
-# Connect to the Cube flight controller
-master = mavutil.mavlink_connection('COM5', baud=9600)
+# master = mavutil.mavlink_connection('192.168.0.101:14551')
+master = mavutil.mavlink_connection('192.168.0.105:14550')
 
-# Wait for the heartbeat message to start receiving data
-master.wait_heartbeat()
-
-# Main loop
 while True:
-    try:
-        # Check system status
-        print("System status: ", mavutil.mavlink.enums['MAV_STATE'][master.messages['HEARTBEAT'].system_status].name)
-
-        # Check battery voltage
-        print("Battery voltage: ", master.messages['SYS_STATUS'].voltage_battery / 1000.0, "V")
-
-        # Check GPS fix status
-        gps_fix = master.messages['GPS_RAW_INT'].fix_type
-        if gps_fix == 3:
-            print("GPS fix: 3D Fix")
-        elif gps_fix == 2:
-            print("GPS fix: 2D Fix")
-        else:
-            print("GPS fix: No fix")
-
-        # Check vehicle mode
-        print("Vehicle mode: ", mavutil.mavlink.enums['MAV_MODE_FLAG'][master.messages['SYS_STATUS'].mode].name)
-
-        time.sleep(1)  # Delay between checks
-
-    except KeyboardInterrupt:
-        break
-
-# Close the connection
-master.close()
+    msg = master.recv_match()
+    if not msg:
+        continue
+    if msg.get_type() == 'HEARTBEAT':
+        print("\n\n*****Got message: %s*****" % msg.get_type())
+        print("Message: %s" % msg)
+        print("\nAs dictionary: %s" % msg.to_dict())
+        # Armed = MAV_STATE_STANDBY (4), Disarmed = MAV_STATE_ACTIVE (3)
+        print("\nSystem status: %s" % msg.system_status)
